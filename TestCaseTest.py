074a3f001afd77f02758501e23c79900d511cb5c
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-01-08 21:39:35 +0900

    :recycle: Replace test of `setUp` with logging

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -12,7 +12,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert (self.test.wasSetUp)
+        assert ("setUp " == self.test.log)
 
 
 TestCaseTest("testRunning").run()
